660b7976721649ab99536a36a0ca688ac84ebfe0
Sangmin Lee <manhasum@naver.com>
2025-05-27 02:46:13 +0900

    총알 똑바로 나가도록 스크립트 수정

--- Assets/Scripts/Enemy/Enemy_FSM.cs
+++ Assets/Scripts/Enemy/Enemy_FSM.cs
@@ -131,9 +131,7 @@ public class Enemy_FSM : MonoBehaviour
 
     public void ShotBulletIns() // 적 유닛 전용. 유저는 레이케이스트 사용
     {
-        GameObject bullet_ = Instantiate(bullet);
-        bullet_.transform.position = shotRocation.position;
-        bullet_.transform.rotation = shotRocation.rotation;
+        GameObject bullet_ = Instantiate(bullet, shotRocation.position, shotRocation.rotation);
     }
 
     /// <summary>
