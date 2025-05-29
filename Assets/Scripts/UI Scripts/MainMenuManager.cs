d74dce709235bb9829c4f64a173e30d3079ec41e
Sangmin Lee <manhasum@naver.com>
2025-05-30 08:12:51 +0900

    씬변환 오류 수정

--- Assets/Scripts/UI Scripts/MainMenuManager.cs
+++ Assets/Scripts/UI Scripts/MainMenuManager.cs
@@ -7,7 +7,7 @@ public class MainMenuManager : MonoBehaviour
     // ���� ���� ��ư Ŭ�� ��
     public void OnStartGame()
     {
-        SceneManager.LoadScene("GameScence"); // �� ���� �÷��� �� �̸����� ����
+        SceneManager.LoadScene("GameScene"); // �� ���� �÷��� �� �̸����� ����
     }
 
     // ���� ���� ��ư Ŭ�� ��
