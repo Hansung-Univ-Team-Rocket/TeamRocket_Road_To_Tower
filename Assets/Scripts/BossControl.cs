576bcc3877532febd53e5f2b48b880a67a862a9f
eyeofsol <sjh991101@gmail.com>
2025-05-30 04:40:52 +0900

    보스 패턴 원래대로 수정

--- Assets/Scripts/BossControl.cs
+++ Assets/Scripts/BossControl.cs
@@ -122,7 +122,7 @@ public class BossControl : MonoBehaviour
             {
                 isAttacking = true;
 
-                int pattern = 4; // Random.Range(0, 5);
+                int pattern = Random.Range(0, 5);
                 switch (pattern)
                 {
                     case 0:
